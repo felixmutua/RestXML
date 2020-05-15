f249a4e3a5689fc2703350f5f0a7c45a8aae7892
Felix Mutua <felixmutua01@gmail.com>
2020-05-15 20:26:16 +0300

    Keys

--- src/main/java/com/xmlrest/api/b2c/XmlRestApplication.java
+++ src/main/java/com/xmlrest/api/b2c/XmlRestApplication.java
@@ -2,16 +2,25 @@ package com.xmlrest.api.b2c;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
-import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLSession;
 import java.util.Objects;
 
 @SpringBootApplication
 public class XmlRestApplication {
+
+
+    private final static String KEYSTORE_PASSWORD = "secret";
+
+    static {
+        System.setProperty("javax.net.ssl.trustStore", Objects.requireNonNull(XmlRestApplication.class.getClassLoader().getResource("clienttruststore.p12")).getFile());
+        System.setProperty("javax.net.ssl.trustStorePassword", KEYSTORE_PASSWORD);
+        System.setProperty("javax.net.ssl.keyStore", Objects.requireNonNull(XmlRestApplication.class.getClassLoader().getResource("clientkeystore.p12")).getFile());
+        System.setProperty("javax.net.ssl.keyStorePassword", KEYSTORE_PASSWORD);
+
+        HttpsURLConnection.setDefaultHostnameVerifier(
+                (hostname, sslSession) -> hostname.equals("localhost"));
+    }
     public static void main(String[] args) {
         SpringApplication.run(XmlRestApplication.class, args);
 
