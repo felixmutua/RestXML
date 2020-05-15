f9d7ed78b6a8fb340c3e8f026d5b3cd6cfdc84f7
Felix Mutua <felixmutua01@gmail.com>
2020-05-15 17:14:58 +0300

    git commit -a

--- src/main/java/com/xmlrest/api/b2c/config/ConnectorConfig.java
+++ src/main/java/com/xmlrest/api/b2c/config/ConnectorConfig.java
@@ -1,37 +1,37 @@
-package com.xmlrest.api.b2c.config;
-
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContextBuilder;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.util.ResourceUtils;
-import org.springframework.web.client.RestTemplate;
-
-import javax.net.ssl.SSLContext;
-
-
-@Configuration
-public class ConnectorConfig {
-
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) throws Exception {
-
-        String allPassword = "tomcat";
-        SSLContext sslContext = SSLContextBuilder
-                .create()
-                .loadKeyMaterial(ResourceUtils.getFile("classpath:keystore.jks"), allPassword.toCharArray(), allPassword.toCharArray()).build();
-
-        HttpClient client = HttpClients.custom()
-                .setSSLContext(sslContext)
-                .build();
-        HttpComponentsClientHttpRequestFactory customRequestFactory = new HttpComponentsClientHttpRequestFactory();
-        customRequestFactory.setHttpClient(client);
-
-        return builder
-                .requestFactory(() -> customRequestFactory)
-                .build();
-    }
-}
+//package com.xmlrest.api.b2c.config;
+//
+//import org.apache.http.client.HttpClient;
+//import org.apache.http.impl.client.HttpClients;
+//import org.apache.http.ssl.SSLContextBuilder;
+//import org.springframework.boot.web.client.RestTemplateBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+//import org.springframework.util.ResourceUtils;
+//import org.springframework.web.client.RestTemplate;
+//
+//import javax.net.ssl.SSLContext;
+//
+//
+//@Configuration
+//public class ConnectorConfig {
+//
+//    @Bean
+//    public RestTemplate restTemplate(RestTemplateBuilder builder) throws Exception {
+//
+//        String allPassword = "tomcat";
+//        SSLContext sslContext = SSLContextBuilder
+//                .create()
+//                .loadKeyMaterial(ResourceUtils.getFile("classpath:keystore.jks"), allPassword.toCharArray(), allPassword.toCharArray()).build();
+//
+//        HttpClient client = HttpClients.custom()
+//                .setSSLContext(sslContext)
+//                .build();
+//        HttpComponentsClientHttpRequestFactory customRequestFactory = new HttpComponentsClientHttpRequestFactory();
+//        customRequestFactory.setHttpClient(client);
+//
+//        return builder
+//                .requestFactory(() -> customRequestFactory)
+//                .build();
+//    }
+//}
