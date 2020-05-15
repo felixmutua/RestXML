dbe358044101aabbe0a74b722512acdc77bb0ff7
Felix Mutua <felixmutua01@gmail.com>
2020-05-15 20:08:06 +0300

    Client keya

--- src/main/java/com/xmlrest/api/b2c/XmlRestApplication.java
+++ src/main/java/com/xmlrest/api/b2c/XmlRestApplication.java
@@ -12,23 +12,6 @@ import java.util.Objects;
 
 @SpringBootApplication
 public class XmlRestApplication {
-
-
-    private final static String KEYSTORE_PASSWORD = "Admin123#@!";
-
-    static {
-        System.setProperty("javax.net.ssl.trustStore", Objects.requireNonNull(XmlRestApplication.class.getClassLoader().getResource("keystore.p12")).getFile());
-        System.setProperty("javax.net.ssl.trustStorePassword", KEYSTORE_PASSWORD);
-        System.setProperty("javax.net.ssl.keyStore", Objects.requireNonNull(XmlRestApplication.class.getClassLoader().getResource("keystore.p12")).getFile());
-        System.setProperty("javax.net.ssl.keyStorePassword", KEYSTORE_PASSWORD);
-
-        HttpsURLConnection.setDefaultHostnameVerifier(
-                new HostnameVerifier() {
-                    public boolean verify(String hostname, SSLSession sslSession) {
-                        return hostname.equals("localhost");
-                    }
-                });
-    }
     public static void main(String[] args) {
         SpringApplication.run(XmlRestApplication.class, args);
 
--- src/test/java/com/xmlrest/api/b2c/XmlRestApplicationTests.java
+++ src/test/java/com/xmlrest/api/b2c/XmlRestApplicationTests.java
@@ -1,6 +1,6 @@
 package com.xmlrest.api.b2c;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
