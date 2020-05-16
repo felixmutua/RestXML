ab3156e4a95b0267a9b900292f157cd225d117bd
Felix Mutua <felixmutua01@gmail.com>
2020-05-16 16:49:42 +0300

    Install Cert

--- src/main/java/com/xmlrest/api/b2c/ApplicationConfiguration.java
+++ src/main/java/com/xmlrest/api/b2c/ApplicationConfiguration.java
@@ -1,10 +1,12 @@
 package com.xmlrest.api.b2c;
 
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContexts;
+import org.apache.http.ssl.TrustStrategy;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,37 +19,59 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.*;
 import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.time.Duration;
 import java.util.Objects;
 
 @Configuration
 public class ApplicationConfiguration {
-    @Bean
-    public RestTemplate getRestTemplate(RestTemplateBuilder restTemplateBuilder)
-            throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException, IOException, CertificateException {
+//    @Bean
+//    public RestTemplate getRestTemplate(RestTemplateBuilder restTemplateBuilder)
+//            throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException, IOException, CertificateException {
+//
+//        KeyStore trustStore= KeyStore.getInstance(KeyStore.getDefaultType());
+//        File file = new File(Objects.requireNonNull(XmlRestApplication.class.getClassLoader().getResource("ca-truststore.jks")).getFile());
+//        FileInputStream stream = new FileInputStream(file);
+//        trustStore.load(stream, "secret".toCharArray());
+//
+//        SSLContext sslcontext = SSLContexts.custom()
+//                .loadTrustMaterial(trustStore, new TrustSelfSignedStrategy())
+//                .build();
+//
+//
+//        CloseableHttpClient httpClient = HttpClients.custom()
+//                .setSSLContext(sslcontext)
+//                .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+//                .build();
+//
+//        HttpComponentsClientHttpRequestFactory customRequestFactory = new HttpComponentsClientHttpRequestFactory();
+//        customRequestFactory.setHttpClient(httpClient);
+//
+//        return restTemplateBuilder
+//                .requestFactory(() -> customRequestFactory)
+//                .setConnectTimeout(Duration.ofSeconds(30))
+//                .setReadTimeout(Duration.ofSeconds(30))
+//                .build();
+//    }
+@Bean
+public RestTemplate restTemplate()
+        throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+    TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
 
-        KeyStore trustStore= KeyStore.getInstance(KeyStore.getDefaultType());
-        File file = new File(Objects.requireNonNull(XmlRestApplication.class.getClassLoader().getResource("ca-truststore.jks")).getFile());
-        FileInputStream stream = new FileInputStream(file);
-        trustStore.load(stream, "secret".toCharArray());
+    SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
+            .loadTrustMaterial(null, acceptingTrustStrategy)
+            .build();
 
-        SSLContext sslcontext = SSLContexts.custom()
-                .loadTrustMaterial(trustStore, new TrustSelfSignedStrategy())
-                .build();
+    SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
 
+    CloseableHttpClient httpClient = HttpClients.custom()
+            .setSSLSocketFactory(csf)
+            .build();
 
-        CloseableHttpClient httpClient = HttpClients.custom()
-                .setSSLContext(sslcontext)
-                .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
-                .build();
+    HttpComponentsClientHttpRequestFactory requestFactory =
+            new HttpComponentsClientHttpRequestFactory();
 
-        HttpComponentsClientHttpRequestFactory customRequestFactory = new HttpComponentsClientHttpRequestFactory();
-        customRequestFactory.setHttpClient(httpClient);
-
-        return restTemplateBuilder
-                .requestFactory(() -> customRequestFactory)
-                .setConnectTimeout(Duration.ofSeconds(30))
-                .setReadTimeout(Duration.ofSeconds(30))
-                .build();
-    }
+    requestFactory.setHttpClient(httpClient);
+    return new RestTemplate(requestFactory);
+}
 }
--- src/main/java/com/xmlrest/api/b2c/client/ServerClientImpl.java
+++ src/main/java/com/xmlrest/api/b2c/client/ServerClientImpl.java
@@ -1,21 +1,30 @@
 package com.xmlrest.api.b2c.client;
 
 import lombok.extern.log4j.Log4j2;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.TrustStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 
 @Log4j2
 @Service
 public class ServerClientImpl implements ServerClient {
 
-    private final RestTemplate restTemplate;
-
-    public ServerClientImpl(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Override
     public String callServer(String xmlPayload) {
@@ -27,16 +36,18 @@ public class ServerClientImpl implements ServerClient {
 
         HttpEntity<?> httpEntity = new HttpEntity<>(xmlPayload, httpHeaders);
 
-        log.info("Headers"+httpEntity.getHeaders());
+        log.info("Headers" + httpEntity.getHeaders());
 
         ResponseEntity<String> response =
                 restTemplate.exchange("https://172.23.115.140:7178/service/b2c", HttpMethod.POST, httpEntity, String.class);
 
-        log.info("Response"+response.getBody());
-        log.info("Code"+response.getStatusCode());
+        log.info("Response" + response.getBody());
+        log.info("Code" + response.getStatusCode());
         return response.getBody();
 
 
     }
 
 }
+
+
