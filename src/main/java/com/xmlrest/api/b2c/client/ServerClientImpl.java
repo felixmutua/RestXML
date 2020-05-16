990d79383fac5de4238c27e937c66d53764180f7
Felix Mutua <felixmutua01@gmail.com>
2020-05-16 14:31:58 +0300

    Install Cert

--- src/main/java/com/xmlrest/api/b2c/client/ServerClientImpl.java
+++ src/main/java/com/xmlrest/api/b2c/client/ServerClientImpl.java
@@ -1,10 +1,7 @@
 package com.xmlrest.api.b2c.client;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.CacheControl;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -28,7 +25,7 @@ public class ServerClientImpl implements ServerClient {
         log.info("Headers"+httpEntity.getHeaders());
         log.info("Body"+httpEntity.getBody());
 
-        return restTemplate.getForObject("https://172.23.115.140:7178/service/b2c", String.class);
+        return restTemplate.postForObject("https://172.23.115.140:7178/service/b2c",httpEntity, String.class);
     }
 
 }
