008bd3fcc18f54cd121e6819df73dd3f8cc0e05d
Felix Mutua <felixmutua01@gmail.com>
2020-05-17 00:05:14 +0300

    new test

--- src/main/java/com/xmlrest/api/b2c/XmlRestApplication.java
+++ src/main/java/com/xmlrest/api/b2c/XmlRestApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-
 @SpringBootApplication
 public class XmlRestApplication {
-    public static void main (String[] args)  {
+            public static void main (String[] args)  {
+
 
         SpringApplication.run(XmlRestApplication.class, args);
+
+
 
 
     }
