dc8fcb585e4cf51be8f4f9472b9610d12cb0bffd
Felix Mutua <felixmutua01@gmail.com>
2020-05-16 23:58:34 +0300

    new test

--- src/main/java/com/xmlrest/api/b2c/SSLConfig.java
+++ src/main/java/com/xmlrest/api/b2c/SSLConfig.java
@@ -10,17 +10,17 @@ import java.util.Objects;
 @Configuration
 public class SSLConfig {
 
-    @Autowired
-    private Environment env;
-
-
-    @PostConstruct
-    private void configureSSL() {
-        //set to TLSv1.1 or TLSv1.2
-        System.setProperty("https.protocols", "TLSv1.2");
-
-        System.setProperty("javax.net.ssl.trustStore", Objects.requireNonNull(env.getProperty("server.ssl.trust-store")));
-        System.setProperty("javax.net.ssl.trustStorePassword", Objects.requireNonNull(env.getProperty("server.ssl.trust-store-password")));
+//    @Autowired
+//    private Environment env;
+//
+//
+//    @PostConstruct
+//    private void configureSSL() {
+//        //set to TLSv1.1 or TLSv1.2
+//        System.setProperty("https.protocols", "TLSv1.2");
+//
+//        System.setProperty("javax.net.ssl.trustStore", Objects.requireNonNull(env.getProperty("server.ssl.trust-store")));
+//        System.setProperty("javax.net.ssl.trustStorePassword", Objects.requireNonNull(env.getProperty("server.ssl.trust-store-password")));
 
     }
 }
