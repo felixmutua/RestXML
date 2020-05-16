96a72e11ac5ba9e4316e544f8feb2da63a0e6ef2
Felix Mutua <felixmutua01@gmail.com>
2020-05-16 23:37:23 +0300

    new test

--- src/main/java/com/xmlrest/api/b2c/SSLConfig.java
+++ src/main/java/com/xmlrest/api/b2c/SSLConfig.java
@@ -5,20 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
 import javax.annotation.PostConstruct;
+import java.util.Objects;
 
 @Configuration
 public class SSLConfig {
-    @Autowired
-    private Environment env;
-
     @PostConstruct
     private void configureSSL() {
         //set to TLSv1.1 or TLSv1.2
-        System.setProperty("https.protocols", "TLSv1.1");
+        System.setProperty("https.protocols", "TLSv1.2");
 
-        //load the 'javax.net.ssl.trustStore' and
-        //'javax.net.ssl.trustStorePassword' from application.properties
-        System.setProperty("javax.net.ssl.trustStore", env.getProperty("server.ssl.trust-store"));
-        System.setProperty("javax.net.ssl.trustStorePassword",env.getProperty("server.ssl.trust-store-password"));
+        System.setProperty("javax.net.ssl.trustStore", Objects.requireNonNull(SSLConfig.class.getClassLoader().getResource("clientkeystore.jks")).getFile());
+        System.setProperty("javax.net.ssl.trustStorePassword","secret" );
+
     }
 }
